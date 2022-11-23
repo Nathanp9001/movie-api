a96910a1bf7b116f45df0a261ab76ec6e3662944
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2022-11-23 01:11:52 -0600

    delete old code

--- index.js
+++ index.js
@@ -262,14 +262,3 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
  console.log('Listening on Port ' + port);
 });
-
-/*
-
-mongoimport --uri mongodb+srv://myFlixDBadmin:adminflix8487@myflixdb.gvd1ypq.mongodb.net/myFlixDB --collection users --type json --file ../../users.json
-
-
-mongosh "mongodb+srv://myflixdb.gvd1ypq.mongodb.net/myFlixDB" --apiVersion 1 --username myFlixDBadmin
-
-mongoexport -d myFlixDB -c genres -o genres.json
-
-*/
