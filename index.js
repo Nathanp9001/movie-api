c3489e38d05b2e6c36abe882a52334b5d03509f3
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2022-11-26 18:34:43 -0600

    remove jwt auth temporarily

--- index.js
+++ index.js
@@ -195,15 +195,26 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 });
 
 // READ Get list of all movies 
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+// app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+//   Movies.find()
+//   .then((movies) => {
+//     res.status(201).json(movies);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     res.status(500).send('Error: ' + err);
+//   });
+// });
+
+app.get("/movies", function (req, res) {
   Movies.find()
-  .then((movies) => {
-    res.status(201).json(movies);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send('Error: ' + err);
-  });
+    .then(function (movies) {
+      res.status(201).json(movies);
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
 // READ Get info about a movie by title 
