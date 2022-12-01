969d93d23ff9bbd88c3992eb94961dd26523f97b
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2022-12-01 17:54:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,22 +17,12 @@ const app = express();
 app.use(bodyParser.json());
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 const bcrypt = require('bcrypt');
 
 const { check, validationResult } = require('express-validator');
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 let auth = require('./auth')(app);
 const passport = require('passport');
